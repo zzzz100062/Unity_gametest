9dfe8d031d39b1bb7046af68d9e7741f2009030b
zzzz100062 <zzzz100062@gmail.com>
2024-07-18 10:51:38 +0800

    Revert "nothing"
    This reverts commit e8c1b5e13d0b9f0565a86a03eb0cae894e1e2366.

---
+++ Assets/mycode/test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Inventory inventorytest;
+    public Item item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*var num = 4;
+        var count = inventorytest.itemList[1].kamiList.Count;
+        count = count + num;
+        Debug.Log(inventorytest.itemList[1].kamiList[0].data.itemName);
+        Debug.Log(count);*/
+
+        item.QQ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
